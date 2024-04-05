cf5b3db0cbe317224b328a278256e62e4fc2cf00
Francisco Carmona <jcarmfran@gmail.com>
2024-04-05 15:16:27 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Restaurant-Bot-App",
+    version = "0.0.0",
+    author = "Cisco",
+    author_email= "jcarmfran@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
